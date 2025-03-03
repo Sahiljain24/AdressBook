cc785e6deb5bce9df8caf3507c9b58ce6fdd0773
Sahiljain24 <24sahilj@gmail.com>
2025-03-03 23:41:33 +0530

    [Sahil] Add. added all the files for uc_02

---
+++ Book/src/main/java/com/Adress/Book/repository/ContactRepository.java
@@ -0,0 +1,10 @@
+package com.Adress.Book.repository;
+
+import com.Adress.Book.model.Contact;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ContactRepository extends JpaRepository<Contact, Long> {
+
+}

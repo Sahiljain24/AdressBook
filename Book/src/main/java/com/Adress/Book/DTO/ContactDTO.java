733144a5f9cbc3a25cf991e98748dd6675212ddd
Sahiljain24 <24sahilj@gmail.com>
2025-03-03 23:47:55 +0530

    [sahil] Add. added DTo

---
+++ Book/src/main/java/com/Adress/Book/DTO/ContactDTO.java
@@ -0,0 +1,16 @@
+package com.Adress.Book.DTO;
+
+
+
+
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ContactDTO {
+    private String name;
+    private String email;
+    private String phoneNumber;
+}
+
